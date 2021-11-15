9dd42791c8c0da3097c0cfefdb4b8ec52f7f099e
soma1809 <60248071+soma1809@users.noreply.github.com>
2021-11-15 16:17:07 +0530

    Update haiku_client.java

--- Haiku-Server-and-Client/haiku_client.java
+++ Haiku-Server-and-Client/haiku_client.java
@@ -1,19 +1,20 @@
+//Import the necessary packages
 import java.io.*;
 import java.net.*;
 
 public class haiku_client{
     public void make_conn(){
 	try{
-	    Socket sock=new Socket("127.0.0.1",5575);
+	    Socket sock=new Socket("127.0.0.1",5575); //Connect to localhost on port 5575
 
-	    InputStream in=sock.getInputStream();
-	    BufferedReader reader=new BufferedReader(new InputStreamReader(in));
+	    InputStream in=sock.getInputStream(); //Get the byte stream data
+	    BufferedReader reader=new BufferedReader(new InputStreamReader(in)); //Read the data as characters through InputStreamReader
 
 	    String line;
 	    while((line=reader.readLine())!=null)
-		System.out.println(line);
+		System.out.println(line); //Print the data obtianed from the stream
 
-	    sock.close();
+	    sock.close(); //Close the connection
 	}
 	catch(IOException ex){
 	    ex.printStackTrace();
@@ -21,6 +22,7 @@ public class haiku_client{
     }
 
     public static void main(String args[]){
+	    //Create a new class object and call make_conn function
 	haiku_client c=new haiku_client();
 	c.make_conn();
     }
