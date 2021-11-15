d254e338dc4b449fa2044e292e6beae9fa6c9a07
soma1809 <60248071+soma1809@users.noreply.github.com>
2021-11-15 16:19:34 +0530

    Update haiku_server.java

--- Haiku-Server-and-Client/haiku_server.java
+++ Haiku-Server-and-Client/haiku_server.java
@@ -1,3 +1,4 @@
+//Import the necessary packages
 import java.io.*;
 import java.net.*;
 import java.util.Scanner;
@@ -5,6 +6,7 @@ import java.util.Random;
 
 public class haiku_server{
 
+	//The list of haiku files (there are 10 of them)
     String[] haiku_list= new String[] {"haiku1.txt","haiku2.txt","haiku3.txt","haiku4.txt",
 			 "haiku5.txt","haiku6.txt","haiku7.txt","haiku8.txt","haiku9.txt","haiku10.txt"};
     int k;
@@ -12,22 +14,22 @@ public class haiku_server{
     public void send_haiku(){
 	
 	try{
-	    ServerSocket serversock=new ServerSocket(5575);
+	    ServerSocket serversock=new ServerSocket(5575); //Create a new socket on the localhost on port 5575
 
 	    while(true){
-		Socket sock=serversock.accept();
+		Socket sock=serversock.accept(); //Accept connections 
 
 		Random rand=new Random();
-		k=rand.nextInt(10);
+		k=rand.nextInt(10); //Generate a random number from 0 to 9
 
-		File file=new File(haiku_list[k]);
-		Scanner sc=new Scanner(file);
+		File file=new File(haiku_list[k]); //Create a file pointer to the selected file
+		Scanner sc=new Scanner(file); //Scanner object to read the file
 
-		PrintWriter writer=new PrintWriter(sock.getOutputStream(),true);
+		PrintWriter writer=new PrintWriter(sock.getOutputStream(),true); 
 		while(sc.hasNextLine())
-		    writer.println(sc.nextLine());
+		    writer.println(sc.nextLine()); //Write the file data to the output stream
 		
-		sock.close();
+		sock.close(); //Close the connection
 	    }
 		
 	}catch(Exception ex){
@@ -35,6 +37,8 @@ public class haiku_server{
 	}
     }
 	public static void main(String []args){
+		
+		//Create a class instance and start the haiku_server 
 	    haiku_server s=new haiku_server();
 	    s.send_haiku();
 	}
